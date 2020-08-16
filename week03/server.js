a78dca850f723ec503d2d61a3af14a8899c49648
gaofengJ <760613910@qq.com>
2020-08-17 00:54:24 +0800

    week3作业完善

--- week03/server.js
+++ week03/server.js
@@ -1,36 +1,18 @@
 const http = require('http')
+const fs = require('fs')
 
 http.createServer((request, response) => {
-  let body = {}
+  let body = []
   request.on('error', err => {
     console.error(err)
   }).on('data', chunk => {
     body.push(chunk.toString)
   }).on('end', () => {
-    body = Buffer.concat(body).toString()
+    body = body.join('')
     console.log('body', body)
     response.writeHead(200, { 'Content-Type': 'text/html' })
-    response.end(
-`<html maa=a >
-<head>
-  <style>
-    body div #myid {
-      width: 100px;
-      background-color: #FF5000;
-    }
-    body div img {
-      width: 30px;
-      background-clor: #FF1111;
-    }
-  </style>
-</head>
-<body>
-  <div>
-    <img id="myid"/>
-    <img />
-  </div>
-</body>
-</html>`)
+    fs.readFileSync('./index.html')
+    response.end(data)
   })
 }).listen(8088)
 
