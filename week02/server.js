a59409168231ceed16e3aaa22826d87cc32a3b1e
gaofengJ <760613910@qq.com>
2020-08-16 22:06:22 +0800

    代码修改

--- week02/server.js
+++ week02/server.js
@@ -9,7 +9,7 @@ http.createServer((request, response) => {
   }).on('end', () => {
     body = Buffer.concat(body).toString()
     console.log('body', body)
-    response.writeHead(200, { 'Content-Type', 'text/html' })
+    response.writeHead(200, { 'Content-Type': 'text/html' })
     response.end('Hello World\n')
   })
 }).listen(8088)
