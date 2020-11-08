337e13ca0f376093733fed7ad3913ce983e199f6
gaofengJ <760613910@qq.com>
2020-11-08 22:05:31 +0800

    week14作业及总结

--- week14/NOTE.md
+++ week14/NOTE.md
@@ -1,1 +1,26 @@
-学习笔记
+## 学习笔记
+
+JavaScript中处理帧的方案：    
+```javascript
+setInterval(() => {}, 16) // 人眼可识别的最高刷新频率为60帧
+
+let tick = () => {
+  setTimeout(() => {}, 16)
+}
+
+let tick = () => {
+  requestAnimationFrame(tick) // 申请浏览器执行下一帧的时候执行以下代码，使用cancelAnimationFrame去掉
+}
+```
+
+动画分为属性动画和帧动画。前端开发中一般都是属性动画。
+
+时间线的更新：  
+1. 在add函数中添加startTime参数，表示animation的开始时间
+2. 在start函数中比较startTime与animation的startTime，来处理动画之前添加的动画和运行中添加的动画
+
+通过cancelAnimationFrame、记录PAUSE_TIME来实现暂停、重启功能。
+
+通过将所有参数状态设为空或重新声明来实现重置功能。
+
+通过state来管理TimeLine状态。
---
+++ week14/jsx/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = {
+  // entry: './main.js',
+  entry: './animation-demo.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader', // ES6转为ES5
+          options: {
+            presets: ['@babel/preset-env'],
+            // plugins: ['@babel/plugin-transform-react-jsx'] // 解析jsx
+            plugins: [['@babel/plugin-transform-react-jsx', { pragma: 'createElement' }]] // 改变解析jsx的默认方法
+          }
+        }
+      }
+    ]
+  },
+  mode: 'development', // 开发环境，生产环境替换成production（开发环境模式不会压缩代码）
+  devServer: {
+    port: 8080,
+    hot: true,
+    // contentBase: path.join(__dirname, '/dist'),
+    contentBase: path.join(__dirname),
+    watchContentBase: true
+  }
+}
